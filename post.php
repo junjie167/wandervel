6daef36a8bc64a930280cefbb6001435f1aeab2c
Jun Jie <gohjunjie72@gmail.com>
2020-11-19 02:11:52 +0800

    havent finish

---
+++ post.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<?php
+include "head.php";
+?>
+
+<head>
+    <!-- Post CSS Files -->
+    <link rel="stylesheet" href="css/post.css">
+</head>
+
+<body>
+    <header>
+        <?php
+        include "navbar.php";
+        ?>
+    </header>
+    <main>
+        <section class="blog-posts grid-system">
+            <div class="container">
+                <div class="center">
+                    <h1 class="header-title">Post</h1>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="blog post">
+                            <div class="blog-image">
+                                <img src="image/astronomy-1867616__340.jpg" alt="astronomy">
+                            </div>
+                            <div class="blog-title">
+                                <h3>Taiwan Trips</h3>
+                            </div>
+                            <div class="blog-content">
+                                Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.
+                            </div>
+                            <div class="blog-footer">
+                                <ul class="post-info">
+                                    <li><i class="material-icons">date_range</i>Novmember 19, 2020</li>
+                                    <li><i class="material-icons">create</i>by Jun Jie</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="blog post">
+                            <div class="blog-image">
+                                <img src="image/astronomy-1867616__340.jpg" alt="astronomy">
+                            </div>
+                            <div class="blog-title">
+                                <h3>Taiwan Trips</h3>
+                            </div>
+                            <div class="blog-content">
+                                Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.
+                            </div>
+                            <div class="blog-footer">
+                                <ul class="post-info">
+                                    <li><i class="material-icons">date_range</i>Novmember 19, 2020</li>
+                                    <li><i class="material-icons">create</i>by Jun Jie</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="blog post">
+                            <div class="blog-image">
+                                <img src="image/astronomy-1867616__340.jpg" alt="astronomy">
+                            </div>
+                            <div class="blog-title">
+                                <h3>Taiwan Trips</h3>
+                            </div>
+                            <div class="blog-content">
+                                Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.
+                            </div>
+                            <div class="blog-footer">
+                                <ul class="post-info">
+                                    <li><i class="material-icons">date_range</i>Novmember 19, 2020</li>
+                                    <li><i class="material-icons">create</i>by Jun Jie</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="blog post">
+                            <div class="blog-image">
+                                <img src="image/astronomy-1867616__340.jpg" alt="astronomy">
+                            </div>
+                            <div class="blog-title">
+                                <h3>Taiwan Trips</h3>
+                            </div>
+                            <div class="blog-content">
+                                Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.
+                            </div>
+                            <div class="blog-footer">
+                                <ul class="post-info">
+                                    <li><i class="material-icons">date_range</i>Novmember 19, 2020</li>
+                                    <li><i class="material-icons">create</i>by Jun Jie</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+<?php
+include "footer.php";
+?>
+
+</html>
