dafb24c06fbd1db6a9f7ca623798af9a38419b8f
Jun Jie <gohjunjie72@gmail.com>
2020-11-21 01:24:44 +0800

    ain edited

--- navbar.php
+++ navbar.php
@@ -1,9 +1,8 @@
 <!-- Header -->
         
 
-            <div class = "container">
+<div class = "container">
                 <nav class="navbar navbar-expand-lg">
-
                     <a class="navbar-brand" href="index.php"><h2>Wandervel<em>.</em></h2></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
@@ -11,30 +10,25 @@
                     <!--<div class="collapse navbar-collapse" id="navbarResponsive">-->
                     <div class="collapse navbar-collapse" id="navbarText">
                         <ul class="navbar-nav ml-auto">
-
-
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Posts</a>
                             </li>
-
                             <li class="nav-item">
                                 <a class="nav-link" href="#">My Favourites</a>
                             </li>
-
                         </ul>
-
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
                                 <a class="nav-link" href="#" title="sign in">                
                                     <i class="material-icons">perm_identity</i>
                                 </a>
+                              <div class="dropdown-menu" aria-labelledby="dropdown1">
+                                    <a class="dropdown-item" href="#">Log In</a>
+                                    <a class="dropdown-item" href="#">Register</a>
+                                    
+                                </div>
                             </li>
                         </ul> 
-
-
                     </div>
-
                 </nav>
             </div>
-        
-
