5516b538ea5c2ed69678c75741f9152a3b9cbfac
potatopootie <celesteong98@hotmail.com>
2020-11-21 16:08:02 +0800

    test2

--- checklogin.php
+++ checklogin.php
@@ -6,29 +6,17 @@ $userfound = 0;
 
 $config = parse_ini_file('../../private/db-config.ini');
 $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-
 if($conn->connect_error){
     echo "<p>Welp cant even connect</p>";
 }else{
+
     $stmt = $conn->prepare("SELECT * FROM user WHERE email = ?");
     $stmt->bind_param("s", $e);
     $stmt->execute();
     $result = $stmt->get_result();
-    
-    $e = "It didnt work either";
-    if($result->num_rows > 0){
-        while($row = $result->fetch_assoc){
-            $role = $row['role'];
-            $e = $row['email'];
-            $userfound = 1;
-        }
-    }
-    
-    echo "<p>" . $role . "</p>";
-    echo "<p>" . $e . "</p>";
+    $userfound = mysqli_num_rows($result);
+
 }
-
-
 
 if ($userfound == 1)
 {
@@ -44,12 +32,12 @@ if ($userfound == 1)
         session_start();
         $_SESSION["email"]=$e;
         $_SESSION["role"]=$role;
-        header("Location:userProfile.php");
+        header("Location:index.php");
     }
-// } else {
+} else {
         
-//         // user record is not FOUND in the user table
-//         header("Location:login.php?fail=1"); // go back to login page
+        // user record is not FOUND in the user table
+        header("Location:login.php?fail=1"); // go back to login page
  }
 
 
