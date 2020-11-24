41ebf6cdcca9662c188b71fdb1be3930bd035f93
Jun Jie <gohjunjie72@gmail.com>
2020-11-25 01:38:06 +0800

    Need to fix ajax bug

--- js/viewpost.js
+++ js/viewpost.js
@@ -1,4 +1,44 @@
 $(document).ready(function(){
 
+    $(document).on('click', '#bookmark', function(){
 
+        var id = $(this).attr("data-id");
+        
+        $.ajax({
+            url: "include/postDB.php",
+            type: "POST",
+            data: {id: id, action: "add_fav_post"},
+            success: function (response) {
+                console.log(response);
+                $("#bookmark-text").text("Added to favourites successfully");
+                $("#fav-modal").css("display","block");
+                $("#bookmark").css("display", "none");
+                $("#unbookmark").css("display", "block");
+                setTimeout(function(){
+                    $("#fav-modal").css("display","none");
+                }, 10000);
+            }
+        });
+    })
+
+    $(document).on('click', '#unbookmark', function(){
+
+        var id = $(this).attr("data-id");
+        $.ajax({
+            url: "include/postDB.php",
+            type: "POST",
+            data: {id: id, action: "remove_fav_post"},
+            dataType: "text",
+            success: function (response) {
+                console.log(response);
+                $("#bookmark-text").text("Removed from favourites successfully");
+                $("#fav-modal").css("display","block");
+                $("#bookmark").css("display", "block");
+                $("#unbookmark").css("display", "none");
+                setTimeout(function(){
+                    $("#fav-modal").css("display","none");
+                }, 10000);
+            }
+        });
+    })
 })
--- viewpost.php
+++ viewpost.php
@@ -8,6 +8,8 @@ include "include/postDB.php";
 ?>
 <head>
     <link rel="stylesheet" href="css/viewpost.css">
+    <script defer src="js/viewpost.js"></script>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
     <body>
@@ -18,7 +20,11 @@ include "include/postDB.php";
         </header>
         <main>
             <section class="blog-posts grid-system">
+                <div id="fav-modal" class="bookmark-modal">
+                    <p id="bookmark-text"></p>
+                </div>
                 <div class="container">
+                    
                     <?php
                         display_post();
                     ?>
@@ -33,5 +39,6 @@ include "include/postDB.php";
         </main>
     </body>
 <?php include "footer.php"; ?>
+
 
 </html>
