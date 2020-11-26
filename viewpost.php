bc0fa3e4e7be890f7536c88079e2e27475b955e6
Jun Jie <gohjunjie72@gmail.com>
2020-11-26 10:45:05 +0800

    added fav

--- viewpost.php
+++ viewpost.php
@@ -20,7 +20,9 @@ include "include/postDB.php";
         </header>
         <main>
             <section class="blog-posts grid-system">
-                
+                <div id="fav-modal" class="bookmark-modal">
+                    <p id="bookmark-text"></p>
+                </div>
                 <div class="container">
                     
                     <?php
