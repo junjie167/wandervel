d2550359d00640752a17e2c586bc2b0e084b32ee
Jun Jie <gohjunjie72@gmail.com>
2020-11-26 10:49:51 +0800

    Added post link

--- indexnavbar.php
+++ indexnavbar.php
@@ -14,7 +14,7 @@
 
 
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Posts</a>
+                                <a class="nav-link" href="post.php?page=1">Posts</a>
                             </li>
 
                            
