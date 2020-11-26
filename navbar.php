ec5e3be91f52cdff5ca03bf770c77cfa277b700d
ainmm00 <munnie2pm@gmail.com>
2020-11-26 10:28:06 +0800

    edited 2

--- navbar.php
+++ navbar.php
@@ -1,8 +1,9 @@
 <!-- Header -->
         
 
-<div class = "container">
+            <div class = "container">
                 <nav class="navbar navbar-expand-lg">
+
                     <a class="navbar-brand" href="index.php"><h2>Wandervel<em>.</em></h2></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
@@ -10,25 +11,36 @@
                     <!--<div class="collapse navbar-collapse" id="navbarResponsive">-->
                     <div class="collapse navbar-collapse" id="navbarText">
                         <ul class="navbar-nav ml-auto">
+
+
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Posts</a>
+                                <a class="nav-link" href="post.php">Posts</a>
                             </li>
+
                             <li class="nav-item">
                                 <a class="nav-link" href="#">My Favourites</a>
                             </li>
+
                         </ul>
+
                         <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" title="sign in">                
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="dropdown1" role="button" data-toggle="dropdown1" aria-haspopup="true" aria-expanded="false">                
                                     <i class="material-icons">perm_identity</i>
                                 </a>
-                              <div class="dropdown-menu" aria-labelledby="dropdown1">
-                                    <a class="dropdown-item" href="#">Log In</a>
-                                    <a class="dropdown-item" href="#">Register</a>
-                                    
+                                <div class="dropdown-menu" aria-labelledby="dropdown1">
+                                    <a class="dropdown-item" href="userProfile.php">My Profile</a>
+                                    <a class="dropdown-item" href="#">My Posts</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Log Out</a>                                                                    
                                 </div>
                             </li>
                         </ul> 
+
+
                     </div>
+
                 </nav>
             </div>
+        
+
