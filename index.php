4e5fee67b74ba648876ab6a53a74ab3a9fa1acb5
ainmm00 <munnie2pm@gmail.com>
2020-11-29 21:13:45 +0800

    index unfinished

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 
+    
     <?php
     include "head.php";
     ?>
-
+    <head>
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    
     <body>
         <header>
         <?php
         include "indexnavbar.php";
         ?>
         </header>
+      
         <section class="blog-posts grid-system">
             <div class="container">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="indeximages/travel1.jpg" alt="First slide">
+                            <div class="carousel-caption">
+                                <h2>Welcome to Wandervel!</h2>
+                                <p>A travel blog for you to share your own travel experience with everyone who loves to travel!</p>
+                            </div>
+                        </div>
+                        <!--<div class="carousel-item">
+                            <img class="d-block w-100" src="indeximages/travel2.jpg" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="indeximages/travel3.jpg" alt="Third slide">
+                        </div>-->
+                    </div>
+                </div>
+               
                 <div class="all-blog-posts">
                     <h2 class="text-center">Blog</h2>
                     <br>
