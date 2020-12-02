978a567727297d98df25a4db472b9147432bea4b
potatopootie <celesteong98@hotmail.com>
2020-12-02 20:05:34 +0800

    validatedsignup

--- signup.php
+++ signup.php
@@ -233,10 +233,10 @@
                         </div>
 
                         <div class="form-group">
-                            <label style="font-size: 20px;" for="male">Gender:</label>
+                            <label style="font-size: 20px;" for="gender">Gender:
                             <input type="radio" id="male" name="gender" value="Male" required><label>Male</label> 
-                           <label style="font-size: 20px;" for="female">Gender:</label>
                             <input type="radio" id="female" name="gender" value="Female" required><label>Female</label>
+                            </label>
                         </div>
 
 
